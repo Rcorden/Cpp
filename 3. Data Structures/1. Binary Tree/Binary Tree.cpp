74789c0e343d72678d1c90a8d5f7513c3ec3e7eb
Richard <rcorden0@gmail.com>
2021-01-25 21:55:45 +0000

    Binary Tree - Removed pch.h
    Binary Tree - Removed pch.h

--- 3. Data Structures/1. Binary Tree/Binary Tree.cpp
+++ 3. Data Structures/1. Binary Tree/Binary Tree.cpp
@@ -1,7 +1,6 @@
 // Binary Tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include "BinaryTreeSearch.h"
 #include <iostream>
 
--- 3. Data Structures/1. Binary Tree/BinaryTreeSearch.cpp
+++ 3. Data Structures/1. Binary Tree/BinaryTreeSearch.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "BinaryTreeSearch.h"
 
 
